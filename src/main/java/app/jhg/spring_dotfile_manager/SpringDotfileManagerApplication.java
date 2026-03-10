157020a96cc82742f4630b9a7ed23795c558de08
John Graham <110198683+jharrisong830@users.noreply.github.com>
2026-03-10 17:06:41 -0400

    initial commit

---
+++ src/main/java/app/jhg/spring_dotfile_manager/SpringDotfileManagerApplication.java
@@ -0,0 +1,13 @@
+package app.jhg.spring_dotfile_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDotfileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDotfileManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/jhg/spring_dotfile_manager/SpringDotfileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.jhg.spring_dotfile_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDotfileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
