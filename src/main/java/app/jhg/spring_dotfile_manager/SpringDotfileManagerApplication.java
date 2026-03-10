5b2115081b4f29cb31d1ba94803ae79d18625d06
John Graham <110198683+jharrisong830@users.noreply.github.com>
2026-03-10 18:37:09 -0400

    deps update and enable commands

--- src/main/java/app/jhg/spring_dotfile_manager/SpringDotfileManagerApplication.java
+++ src/main/java/app/jhg/spring_dotfile_manager/SpringDotfileManagerApplication.java
@@ -2,8 +2,10 @@ package app.jhg.spring_dotfile_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.core.command.annotation.EnableCommand;
 
 @SpringBootApplication
+@EnableCommand
 public class SpringDotfileManagerApplication {
 
 	public static void main(String[] args) {
