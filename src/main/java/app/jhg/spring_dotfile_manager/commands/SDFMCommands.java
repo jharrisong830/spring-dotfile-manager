999199da720c3e4f6c1b1dda7e0f5bbcf664a028
John Graham <110198683+jharrisong830@users.noreply.github.com>
2026-03-10 20:01:01 -0400

    commands init

---
+++ src/main/java/app/jhg/spring_dotfile_manager/commands/SDFMCommands.java
@@ -0,0 +1,13 @@
+package app.jhg.spring_dotfile_manager.commands;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.shell.core.command.annotation.Command;
+
+@Configuration
+public class SDFMCommands {
+    
+    @Command("init")
+    public void init() {
+        // TODO: initialize a config file with user input, default otherwise
+    }
+}
