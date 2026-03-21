43d0ed48f56b5a36f11e41c370a9be0e10ae5fd1
John Graham <110198683+jharrisong830@users.noreply.github.com>
2026-03-20 20:49:03 -0400

    remove extra import

--- src/main/java/app/jhg/spring_dotfile_manager/commands/InitCommand.java
+++ src/main/java/app/jhg/spring_dotfile_manager/commands/InitCommand.java
@@ -1,7 +1,6 @@
 package app.jhg.spring_dotfile_manager.commands;
 
 import app.jhg.spring_dotfile_manager.service.ConfigService;
-import app.jhg.spring_dotfile_manager.service.DotfileService;
 import app.jhg.spring_dotfile_manager.util.FormattingUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
